1133b4e821db65838112e0504a6558664b67e864
bravepower <bravepower@gmail.com>
2015-01-24 13:41:48 -0800

    Wall scale prototype scene added

---
+++ Game/Assets/Prototype/Scripts/WallResize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallResize : MonoBehaviour {
+
+	private float currentScale = Vector3.one;
+	private const float GROWTH_FACTOR = 1.01f;
+
+	private float timer = 0.0f;
+
+	void Update()
+	{
+		
+	}
+}
