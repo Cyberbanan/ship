ac7e577e794b6d2a8bcc919f546e92f54efa37ef
seanyliu <sean.y.liu@gmail.com>
2015-01-24 14:05:32 -0800

    enemy

--- Game/Assets/Prototype/Scripts/EnemyMovement.cs
+++ Game/Assets/Prototype/Scripts/EnemyMovement.cs
@@ -4,31 +4,68 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 
 	private float accelerationAmount = 1.0f;
-	private float maxSpeed = 1.0f;
+	public float maxSpeed = 0.5f;
 	private float maxSpeedSqr;
+
+	public float randomizedMovementFrequency = 0.3f;
+
+	// what the enemy wants to move toward
 	public GameObject targetObject;
 
-	void Awake()
-	{
+	// used by the enemy's pathfinding (follow walls)
+	private bool isColliding = false;
+
+	void Awake() {
 		maxSpeedSqr = maxSpeed * maxSpeed;
 	}
 
-	void Start()
-	{
-		
-	}
+	void FixedUpdate() {
+		Vector3 directionToTravel = new Vector3(0.0f, 0.0f, 0.0f);
 
-	void FixedUpdate()
-	{
-		Vector3 directionOfTarget = targetObject.transform.position - transform.position;
-		print (directionOfTarget);
-		directionOfTarget = directionOfTarget.normalized;
-		
-		Vector3 acceleration = new Vector3(directionOfTarget.x * accelerationAmount, 0.0f, directionOfTarget.z * accelerationAmount);
-		rigidbody.AddForce(acceleration, ForceMode.Impulse);
+		if (!isColliding) {
+			// don't need to path find, so move toward the target
+			directionToTravel = (targetObject.transform.position - transform.position).normalized;
+			Debug.DrawRay(transform.position, directionToTravel * 10, Color.white);
+		}
 
-		if(rigidbody.velocity.sqrMagnitude > maxSpeedSqr) {
+		if (Random.value <= randomizedMovementFrequency) {
+			// Add a random force
+			Vector3 randomDirection = new Vector3 (Random.value - 0.5f, 0.0f, Random.value - 0.5f).normalized;
+			directionToTravel += randomDirection;
+			Debug.DrawRay(transform.position, randomDirection * 10, Color.yellow);
+		}
+
+		// add the forces
+		Vector3 acceleration = new Vector3 (directionToTravel.x * accelerationAmount, 0.0f, directionToTravel.z * accelerationAmount);
+		rigidbody.AddForce (acceleration, ForceMode.Impulse);
+
+		// Cap the speed
+		if (rigidbody.velocity.sqrMagnitude > maxSpeedSqr) {
 			rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
 		}
 	}
+
+	void OnCollisionStay(Collision collisionInfo) {
+		// Goal is to move parallel to the wall
+		isColliding = true;
+
+		foreach (ContactPoint contact in collisionInfo.contacts) {
+			// get a parallel angle to the wall
+			Vector3 dir = contact.normal.normalized;
+			Vector3 rotateAway = Quaternion.Euler(0, -90, 0) * dir;
+
+			Debug.DrawRay(transform.position, rotateAway * 10, Color.red);
+
+			// add the force and cap speed
+			rigidbody.AddForce(rotateAway.x * 5.0f * accelerationAmount, 0.0f, rotateAway.z * 5.0f * accelerationAmount);
+			if (rigidbody.velocity.sqrMagnitude > maxSpeedSqr) {
+				rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+			}
+		}
+	}
+
+	void OnCollisionExit(Collision collisionInfo) {
+		isColliding = false;
+	}
+
 }
