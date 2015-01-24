6c8a6c9e3d9f5f4d0bbd820749f187d4e73a42b6
bravepower <bravepower@gmail.com>
2015-01-24 13:43:27 -0800

    Wall resize fix

--- Game/Assets/Prototype/Scripts/WallResize.cs
+++ Game/Assets/Prototype/Scripts/WallResize.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class WallResize : MonoBehaviour {
 
-	private float currentScale = Vector3.one;
+	private float currentScale = 1.0f;
 	private const float GROWTH_FACTOR = 1.01f;
 
 	private float timer = 0.0f;
