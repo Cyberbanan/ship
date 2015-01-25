d02171ed9974a25a7ad9376487ce183443244002
Caleb Leak <caleb.leak@gmail.com>
2015-01-25 00:14:09 -0800

    Added basic title scene and some other people's levels

---
+++ Game/Assets/Scripts/FadeFlash.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeFlash : MonoBehaviour {
+
+	public float FadeTimeScale = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public float currentOpacity = 0;
+
+	// Update is called once per frame
+	void Update () {
+		var mesh = GetComponent<MeshRenderer> ();
+		currentOpacity = (Mathf.Cos (Time.time / FadeTimeScale) + 1) / 2.0f; 
+		mesh.material.color = new Color (0, 0, 0, currentOpacity);
+	}
+}
---
+++ Game/Assets/Scripts/TitleController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour {
+	public GameObject Title;
+	public HackyTextFade UpperText;
+	public HackyTextFade LowerText;
+	public float TimeAutoStart = 10.0f;
+
+	enum State
+	{
+		Title,
+		IntroText
+	}
+
+	State state = State.Title;
+	float levelStartTimer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (state == State.Title)
+		{
+			if (Input.anyKeyDown)
+			{
+				state = State.IntroText;
+				Title.SetActive(false);
+				UpperText.Reset();
+				LowerText.Reset();
+				levelStartTimer = Time.time + TimeAutoStart;
+			}
+		}
+		else
+		{
+			if (Time.time > levelStartTimer)
+			{
+				Application.LoadLevel("Level");
+			}
+		}
+	}
+}
