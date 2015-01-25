16875d8c97dadac8bb6b546422312d1a83e2710e
bravepower <bravepower@gmail.com>
2015-01-24 22:06:33 -0800

    Screen fade started

---
+++ Game/Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFade : MonoBehaviour {
+
+	private static ScreenFade instance = null;
+	public static ScreenFade main
+	{
+		get { return instance; }
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
