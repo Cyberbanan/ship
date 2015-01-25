d87ad9b231cc7949d7b127e3b8a1005a62f26acb
Caleb Leak <caleb.leak@gmail.com>
2015-01-24 18:56:15 -0800

    Added level importer.

---
+++ Game/Assets/Editor/LevelImporterEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(LevelImporter))]
+public class LevelImporterEditor : Editor {
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		
+		LevelImporter importer = (LevelImporter)target;
+		if(GUILayout.Button("Import Level"))
+		{
+			importer.Import();
+		}
+	}
+}
