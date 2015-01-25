af3ecb035f5ad5da8a8d472fae18e58e17de2f3f
bravepower <bravepower@gmail.com>
2015-01-24 21:03:29 -0800

    Openable doors

---
+++ Game/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpen : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
